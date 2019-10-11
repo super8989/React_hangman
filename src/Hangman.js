7debba0d1e02c4ba5d0e801530e8ce022a70b4cc
Sam <super8989@gmail.com>
2019-10-10 20:11:03 -0400

    boilerplate for hangman game

---
+++ src/Hangman.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Hangman extends Component {
+    render() {
+        return (
+            <div>Hangman Game</div>
+        )
+    }
+}
+
+export default Hangman;
