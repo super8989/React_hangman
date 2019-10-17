db07925d1e431613dd16266b26817c8025d77dba
Sam <super8989@gmail.com>
2019-10-16 21:36:13 -0400

    added randomWord to generate a random word for the game

--- src/Hangman.js
+++ src/Hangman.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { randomWord } from './words';
 
 import "./Hangman.css";
 
@@ -19,7 +20,7 @@ class Hangman extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { nWrong: 0, guessed: new Set(), answer: 'apple' };
+        this.state = { nWrong: 0, guessed: new Set(), answer: randomWord() };
         this.handleGuess = this.handleGuess.bind(this);
     }
 
